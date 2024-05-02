37c99df99186c5db21fc664ec4bdc9c05d403695
mercurioregon <jamesm620@gmail.com>
2024-05-02 16:11:36 -0700

    seeds add

--- seeds.sql
+++ seeds.sql
@@ -4,7 +4,9 @@ INSERT INTO employee( first_name, last_name, role_id, manager_id)
   VALUES ("Bob", "Jones", 1, 12);
 
 INSERT INTO roles (id, title, salary, department_id)
-  VALUES (2, "Manager", 100000.00, 1);
+  VALUES (3, "Manager", 100000.00, 1),
+          (5, "CEO", 200000.00, 2),
+          (7, "Shepherd", 2500000.00, 3);
 
 INSERT INTO department (id,name)
   VALUES (2, "Payroll");
